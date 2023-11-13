388caf9a307c3a9a02a12d699a3226dce54c2196
Bhumir2503 <bhumir2503@gmail.com>
2023-11-13 12:59:30 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/website-resume-react/",
+  base: "Website-Resume-React",
   plugins: [react()],
 })
