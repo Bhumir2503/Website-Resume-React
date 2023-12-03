bc48bbaf9af192ab5f28028c4f2289cb31c31f66
Bhumir2503 <36862986+Bhumir2503@users.noreply.github.com>
2023-12-03 11:47:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Website-Resume-React/",
+  base: "/Website-Resume-V1/",
   plugins: [react()],
 })
